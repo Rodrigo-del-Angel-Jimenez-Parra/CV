36233fba500a2b804364818d1a5502bf29db6509
RodrigodelAngel JimenezParra <ingeniero_rodrigo_del_angel_jimenez_parra@outlook.com>
2022-08-21 22:03:24 -0500

    Agregue varios archivos del curso

---
+++ Java/code2dataname.java
@@ -0,0 +1,29 @@
+public class code2dataname 
+{
+    public static void main(String[] args) 
+    {
+        //byte short int long son variables tipo entero
+        //double y float pueden almacenar numeros reales o con punto decimal
+        byte a;
+        a=127;
+        System.out.println("byte= "+a);
+        short b;
+        b=32767;
+        System.out.println("short= "+b); 
+        int c;
+        c=2147483647;
+        System.out.println("int= "+c);
+        char d;
+        d= 'c';
+        System.out.println("char= "+d);
+        boolean e;
+        e=true;
+        System.out.println("boolean= "+e);
+        String f;
+        f="cadena xd";
+        System.out.println("String= "+f);
+        float g;
+        g=3.1416f;
+        System.out.println("float= "+g);
+    }
+}
